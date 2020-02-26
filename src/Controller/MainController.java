9e36bff9a6bad0ca72ec2d07f8384b680d1a41be
palakjain11101 <57346289+palakjain11101@users.noreply.github.com>
2020-02-26 19:09:06 +0000

    line graph added

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -2,15 +2,31 @@ package Controller;
 
 import Model.MainModel;
 import View.MainView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 
 public class MainController {
     private MainView view;
     private MainModel model;
-
+    @FXML
+    LineChart<String, Number> linechart;
     public MainController(MainView view, MainModel model){
         this.view = view;
         this.model = model;
         System.out.println("init controller");
         System.out.println("HELLO");
+    }
+
+    public void btn(ActionEvent event){
+        linechart.getData().clear();
+        XYChart.Series<String, Number> series = new XYChart.Series<String, Number>();
+        series.getData().add(new XYChart.Data<String, Number>("Jan", 200));
+        series.getData().add(new XYChart.Data<String, Number>("Feb", 100));
+        series.getData().add(new XYChart.Data<String, Number>("Mar", 300));
+        series.getData().add(new XYChart.Data<String, Number>("Apr", 230));
+        series.setName("Month Pay");
+        linechart.getData().add(series);
     }
 }
