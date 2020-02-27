4e5a890e8e403d7c9e33f6608744d69586fa9be1
Mardy445 <43888095+Mardy445@users.noreply.github.com>
2020-02-27 21:59:03 +0000

    Minor modifications to SQL file

--- src/View/MainView.java
+++ src/View/MainView.java
@@ -8,9 +8,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,6 +20,7 @@ import java.io.IOException;
 public class MainView extends Application {
 
     Stage stage;
+    Parent root;
 
     public static void main(String[] args) {
         launch(args);
@@ -25,7 +28,6 @@ public class MainView extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root;
         //root = FXMLLoader.load(getClass().getResource("/View/sample.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/View/sample.fxml"));
         root = fxmlLoader.load();
@@ -34,12 +36,38 @@ public class MainView extends Application {
         controller.setModel(new MainModel());
 
         stage = primaryStage;
+        setUpTimeGranulationSlider();
 
         primaryStage.setTitle("Dashboard");
         root.getStylesheets().add("/View/styles.css");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.setMaximized(true);
         primaryStage.show();
+    }
+
+    private void setUpTimeGranulationSlider(){
+        Slider slider = (Slider) root.lookup("#timeGranulationSlider");
+        slider.setLabelFormatter(new StringConverter<Double>() {
+            @Override
+            public String toString(Double d) {
+                switch (d.intValue()){
+                    case 0:
+                        return "Days";
+                    case 1:
+                        return "Weeks";
+                    case 2:
+                        return "Months";
+                    case 3:
+                        return "Years";
+                }
+                return "";
+            }
+
+            @Override
+            public Double fromString(String s) {
+                return null;
+            }
+        });
     }
 
     public File showFileChooser(){
