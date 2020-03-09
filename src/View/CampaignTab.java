f412451423f10f347471bf9d4514ca5a43d4e10e
Mardy445 <43888095+Mardy445@users.noreply.github.com>
2020-03-09 15:28:51 +0000

    Added testing button for our own benefit.

--- src/View/CampaignTab.java
+++ src/View/CampaignTab.java
@@ -4,10 +4,13 @@ import Controller.MainController;
 import Model.GraphPoint;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
 import java.awt.*;
@@ -56,19 +59,32 @@ public class CampaignTab extends Tab {
         table.setEditable(false);
 
         addItems();
+        setSelectionModel();
 
         pane.getChildren().add(table);
 
         //TEMPORARY
-        Button button = new Button();
+        Button button = new Button("TEST");
+        button.setOnMouseClicked(mouseEvent -> controller.onTestButtonPressed());
+        pane.getChildren().add(button);
+    }
+
+    public void updateData(ArrayList<CampaignTab.CampaignDataPackage> newBasicMetrics){
+        this.basicMetrics = newBasicMetrics;
+        addItems();
     }
 
     private void addItems(){
+        table.getItems().clear();
         for(CampaignDataPackage data : basicMetrics){
             table.getItems().add(data);
         }
 
         //table.getSelectionModel().selectFirst();
+
+    }
+
+    private void setSelectionModel(){
         table.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
             controller.metricSelectedOnCampaignTab((CampaignDataPackage) newValue,"test");
         });
