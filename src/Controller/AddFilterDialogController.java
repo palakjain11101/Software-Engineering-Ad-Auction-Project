8490c2368fc12b57b16d2f2ecdfc3083042c36bf
Mardy445 <43888095+Mardy445@users.noreply.github.com>
2020-03-09 15:09:14 +0000

    Added changes to add filter dialog.

--- src/Controller/AddFilterDialogController.java
+++ src/Controller/AddFilterDialogController.java
@@ -1,4 +1,46 @@
 package Controller;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.skin.DatePickerSkin;
+import javafx.scene.input.MouseEvent;
+
+import java.time.LocalDate;
+
 public class AddFilterDialogController {
+
+    @FXML
+    ListView addFilterDialogListView;
+
+    public AddFilterDialogController(){
+    }
+
+    public void setUpDialogController(){
+        DatePicker dateBeforePicker = new DatePicker();
+        DatePicker dateAfterPicker = new DatePicker();
+        ComboBox ageComboBox = new ComboBox();
+        ComboBox genderComboBox = new ComboBox();
+        ComboBox contextComboBox = new ComboBox();
+        ComboBox incomeComboBox = new ComboBox();
+
+        ageComboBox.getItems().addAll("<25","25-34","35-44","45-54",">55");
+        genderComboBox.getItems().addAll("Male","Female");
+        contextComboBox.getItems().addAll("News","Shopping","Social Media","Blog","Hobbies","Travel");
+        incomeComboBox.getItems().addAll("Low","Medium","High");
+
+
+        addFilterDialogListView.getItems().add(dateBeforePicker);
+        addFilterDialogListView.getItems().add(dateAfterPicker);
+        addFilterDialogListView.getItems().add(ageComboBox);
+        addFilterDialogListView.getItems().add(genderComboBox);
+        addFilterDialogListView.getItems().add(contextComboBox);
+        addFilterDialogListView.getItems().add(incomeComboBox);
+
+    }
+
 }
